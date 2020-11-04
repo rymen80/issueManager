07c17628d11616914214f9f59a5661a7b08f3c2e
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-04 15:43:31 -0800

    conncect the Textfields with redux form

--- client/src/pages/Viewer/ViewerViews/SignIn.jsx
+++ client/src/pages/Viewer/ViewerViews/SignIn.jsx
@@ -11,18 +11,22 @@ import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import issUseLogo from '../../../images/issUse.png';
-import SettingsIcon from '@material-ui/icons/Settings';
-import {LoginPagesCopyright} from '../../../pages/common/components/LoginPagesCopyright'
+import issUseLogo from "../../../images/issUse.png";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { LoginPagesCopyright } from "../../../pages/common/components/LoginPagesCopyright";
 
-
+const TextFieldInput = ({ input, meta, label, ...custom }) => {
+  console.log("FIELD COMPONENT PROPS", custom);
+  return <TextField {...input} label={label} meta={meta} {...custom} />;
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
   },
   image: {
-    backgroundImage: "url(https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1470790376778-a9fbc86d70e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=949&q=80)",
     backgroundRepeat: "no-repeat",
     backgroundColor:
       theme.palette.type === "light"
@@ -30,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
         : theme.palette.grey[900],
     backgroundSize: "cover",
     backgroundPosition: "center",
-    opacity: .6,
+    opacity: 0.6,
   },
   paper: {
     margin: theme.spacing(8, 4),
@@ -40,8 +44,8 @@ const useStyles = makeStyles((theme) => ({
   },
   avatar: {
     margin: theme.spacing(8),
-    
-    width: "80%",
+
+    width: "60%",
   },
   form: {
     width: "100%", // Fix IE 11 issue.
@@ -49,20 +53,11 @@ const useStyles = makeStyles((theme) => ({
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    background: '#638FBC',
-    color: 'white',
+    background: "#638FBC",
+    color: "white",
   },
 }));
 
-// What Redux form does for us
-// It will write the functions for updating form state
-// It will also write state to determine the current state of each field
-// It also gives us a function for getting the values out of the input
-// and then putting it in out submit function
-
-//what handleSubmit will do is pass the forms Values as the first parameter
-// handleSubmit also preventsDefault for us right away
-// to the function that it's calling
 const SignIn = (props) => {
   const classes = useStyles();
   const { handleSubmit, history } = props;
@@ -70,7 +65,7 @@ const SignIn = (props) => {
 
   console.log(props);
   const handleSignIn = async (formValues, dispatch) => {
-    console.log(formValues);
+    // console.log(formValues);
     //{ username: 'Your enterereduseRName', password: 'your password' }
     try {
       const res = await axios.post("/auth/signin", formValues);
@@ -87,28 +82,26 @@ const SignIn = (props) => {
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-      <Button
-        variant="outlined"
-        color="#ff9961"
-        className={classes.button}
-        startIcon={<SettingsIcon />}
-        onClick={() => {
-          history.push("/adminsignin")}}
-          style={{right:"0px"}}
-          
-      >
-        Admin
-      </Button>
+        <Button
+          variant="outlined"
+          color="inherit"
+          className={classes.button}
+          startIcon={<SettingsIcon />}
+          onClick={() => {
+            history.push("/adminsignin");
+          }}
+          style={{ position: "sticky",right:"0px",position:'-webkit-sticky',left:"80%",top:"10px" }}
+        >
+          Admin
+        </Button>
         <div className={classes.paper}>
-        
-            <img src={issUseLogo} className={classes.avatar}/>
-        
-         
+          <img src={issUseLogo} className={classes.avatar}/>
+
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
           <form noValidate autoComplete="off" className={classes.form}>
-            <TextField
+            <Field
               variant="outlined"
               margin="normal"
               required
@@ -117,9 +110,9 @@ const SignIn = (props) => {
               name="username"
               label="username"
               autoComplete="username"
-              autoFoucs
+              component={TextFieldInput}
             />
-            <TextField
+            <Field
               variant="outlined"
               margin="normal"
               required
@@ -128,7 +121,9 @@ const SignIn = (props) => {
               label="password"
               autoComplete="password"
               type="password"
-            />
+              name="password"
+              component={TextFieldInput}
+            />            
             <Button
               onClick={handleSubmit(handleSignIn)}
               type="submit"
@@ -141,7 +136,7 @@ const SignIn = (props) => {
             <Box mt={5}>
               <LoginPagesCopyright />
             </Box>
-           </form>
+          </form>
         </div>
       </Grid>
     </Grid>
