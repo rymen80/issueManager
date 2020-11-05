b9b1539d51bcc99fd8f29471c613126b178d4b14
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-05 12:49:25 -0800

    add swagger definition to the index.js

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
+// *** Dot env
 require('dotenv')
   .config();
 const express = require('express');
 const routes = require('./routes');
+// *** Swagger imports
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
 
 require('./services/passport');
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    // openapi: '3.0.0', // *** Specification (optional, defaults to swagger: '2.0')
+    info: {
+      title: 'IssUse Api Document', // *** (required)
+      version: '1.0.0', // *** (required)
+      description: `IssUse Api Information and Tester.
+      IssUse is a simple Issue Management System for your project.This swagger document will help you get acquainted with IssUse APIs, their parameters, description, responses. 
+      You can also test the APIs using these api-docs`      
+    },
+  },
+  // *** Provide the routes for API Documentation
+  apis: ['./routes/apiRoutes/issuesRoutes/index.js','./routes/authRoutes/index.js'],
+};
+
+// *** Custom CSS for the Swagger Documentation
+const swaggerCustomOptions = {  
+  customCss: `
+  .topbar-wrapper img {content:url(https://github.com/rymen80/issueManager/raw/master/client/src/images/issUseLogo.png); width:40px; height:50px;}
+  .swagger-ui .topbar { background-color: #000000; border-bottom: 20px solid #5dc6d1; color: #ff9961}
+  .topbar-wrapper:after{content:"IssUse Api | Created By IssUse Team : William, Chris, Abhijeet, Ryan, Lindsey";display:inline-block}`,
+  customSiteTitle: "IssUse APIs",
+  explorer: false,
+};
+// *** Use Swagger Options
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+// *** Use the swaggerUi.serve middlewares
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs,swaggerCustomOptions));
+
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -18,5 +53,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('Server started listening on PORT http://localhost:3001');
+  console.log(`Server started listening on PORT : ${PORT}`);
 });
