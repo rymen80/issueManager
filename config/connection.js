fc33b6d9fec18469e7daea4c17b2ec1dab2ea583
Ryan Mendez <rjmendez80@gmail.com>
2020-11-04 18:04:37 -0800

    fix bug in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@ const mysql = require('mysql2');
 let connection;
 
 if (process.env.NODE_ENV === 'production') {
-  connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
-  console.log("connected to jaws database");
+  connection = mysql.createConnection(process.env.Jaws_URL).promsie();
+  console.log("connected to JAWSDB");
+
+} else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'fwitter_db',
+     host: 'localhost',
+     user: 'root',
+     password: 'password',
+     database: 'fwitter_db',
   }).promise();
+
 }
+
+
 module.exports = connection;
