453c6d18aea2c9323ca4daf1135451750272acff
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-05 12:59:14 -0800

    add swagger definitions and documentation comments

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
 const authRoutes = require('./authRoutes');
-// /api/users
+
+// *** /api Route
 router.use('/api', apiRoutes);
+
+// *** /auth Route
 router.use('/auth', authRoutes);
 
 module.exports = router;
