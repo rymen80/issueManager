6e04b5f8f6eba626087c5f89c6ffea09e747fc87
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-07 05:44:34 -0800

    update issue Queries

--- model/issuesQueries.js
+++ model/issuesQueries.js
@@ -1,19 +1,21 @@
-const getAllIssuesQuery = 'SELECT * FROM issue;';
-const getIssueByIdQuery = 'SELECT * FROM issue WHERE id = ?;';
-const getIssueByIssueKeyQuery = 'SELECT * FROM issue WHERE issue_key = ?;';
+const getAllIssuesQuery = "SELECT * FROM issue_view;";
+const getAllIssuesInProjectByIdQuery =
+  "SELECT * FROM issue_view WHERE project_id = ? ;";
+const getIssueByIdQuery = "SELECT * FROM issue_view WHERE id = ?;";
+const getIssueByIssueKeyQuery = "SELECT * FROM issue_view WHERE issue_key = ?;";
 
 const createIssueQuery = `
-INSERT INTO issue (issue_key,summary,description,reported_by,assigned_to,status,priority,
-modified_on,modified_by,project_id,resolution_id,label)
-VALUES (?,?,?,?,?,?,?,?,?,?,?,?);`;
+INSERT INTO issue (summary,description,reported_by,assigned_to,status,priority,project_id,resolution_id,label)
+VALUES (?,?,?,?,?,?,?,?,?);`;
 
-const deleteIssueByIdQuery = 'DELETE FROM issue WHERE ID = ?;';
+const deleteIssueByIdQuery = "DELETE FROM issue WHERE ID = ?;";
 
 module.exports = {
   //SELECTS
   getAllIssuesQuery,
   getIssueByIdQuery,
   getIssueByIssueKeyQuery,
+  getAllIssuesInProjectByIdQuery,
 
   //INSERT
   createIssueQuery,
