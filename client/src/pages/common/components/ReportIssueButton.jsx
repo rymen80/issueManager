8c411fa49ff2c82beb29f886cd6561b4737094ff
william jacobs <williamjacobs@williams-MacBook-Pro.local>
2020-11-07 10:25:41 -0800

    create navbar user, project selector, admin transfer, report bug button compponents

---
+++ client/src/pages/common/components/ReportIssueButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import BugReportIcon from '@material-ui/icons/BugReport';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function IconLabelButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<BugReportIcon />}
+      >
+        Report
+      </Button>
+      </div>
+  );
+}
