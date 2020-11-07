b01d171ce1b881c9e97f3e6040013ae0e988fe40
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-07 05:40:36 -0800

    complete issue post

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const issuesRoutes = require('./issuesRoutes');
 const projectRoutes = require('./projectRoutes');
+const userRoutes = require('./userRoutes');
 
 // ** /issues route
 router.use('/issues',issuesRoutes);
 
 // ** /project route
 router.use('/projects',projectRoutes);
+
+// ** /User route
+router.use('/users',userRoutes);
 
 module.exports = router;
--- routes/apiRoutes/projectRoutes.js
+++ routes/apiRoutes/projectRoutes.js
@@ -11,6 +11,55 @@ const router = require('express')
   .Router();
 
 // *** /api/projects
+/**
+ * @swagger
+ * /api/projects:
+ *  get:
+ *    description: Get a list of projects to which user belongs
+ *    summary: Get a list of projects to which user belongs
+ *    tags:
+ *      - Projects
+ *    parameters:
+ *      - name: userid
+ *        in: query
+ *        type: integer
+ *        required: true
+ *        description: Numeric user id 
+ *    responses:
+ *      200:
+ *        description: Status Ok  
+ *  post:
+ *    description: Create a new *project*
+ *    summary: Create a new project
+ *    consumes:
+ *      - application/x-www-form-urlencoded
+ *    tags:
+ *      - Projects
+ *    parameters:      
+ *      - name: name
+ *        in: formData
+ *        type: string        
+ *        required: true
+ *        description: project Name String
+ *      - name: key
+ *        in: formData
+ *        type: string
+ *        required: true
+ *        description: A short 3 letter key to identify project
+ *      - name: description
+ *        in: formData
+ *        type: string
+ *        required: true
+ *        description: Description of project
+ *      - name: created_by
+ *        in: formData
+ *        type: integer
+ *        required: true
+ *        description: UserId of user creating this project
+ *    responses:
+ *      200:
+ *        description: Status Ok  
+ */       
 router.route('/')
 .get(getAllProjectsAPI)
 .post(insertProjectAPI)
@@ -18,6 +67,24 @@ router.route('/')
 
 
 // *** e.g /api/project/1 where 1 is project_id
+/**
+ * @swagger
+ * /api/projects/{id}:
+ *  get:
+ *    description: Get a Project by Project Id
+ *    summary: Get a Project by Project Id
+ *    tags:
+ *      - Projects
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        #required:true     
+ *        schema:
+ *          type:integer
+ *    responses:
+ *      200:
+ *        description: Status Ok        
+ */
 router.route('/:projectId')
 .get(getProjectByIdAPI)
 .delete(deleteProjectByIdApi);
