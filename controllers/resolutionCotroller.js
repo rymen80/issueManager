e1caea4d59806d604cf0b0dea8d1b4738f57b381
Ryan Mendez <rjmendez80@gmail.com>
2020-11-11 18:13:46 -0800

    add resolution controller

---
+++ controllers/resolutionCotroller.js
@@ -0,0 +1,37 @@
+const {
+    fetchAllResolutions,
+    fetchAllResolutionsByResolutionID,
+} = require('../model/resolutionOrm');
+
+
+async function getAllResolutionsAPI(req,res) {
+
+    console.log(req.query);
+    try {
+        let resolutions = await fetchAllResolutions();
+        res.json(resolutions);
+
+    } catch (e) {
+        res.status(400).json(e);
+    }
+}
+
+
+
+async function getResolutionsByResolutionIdAPI (req,res) {
+    const { resolutionId } = req.params;
+    console.log(req.query);
+    try {
+        let resolutionById = await fetchAllResolutionsByResolutionID(resolutionId);
+
+    } catch (e) {
+        res.status(400).json(e);
+    }
+}
+
+module.exports = {
+    getAllResolutionsAPI,
+    getResolutionsByResolutionIdAPI,
+}
+
+
