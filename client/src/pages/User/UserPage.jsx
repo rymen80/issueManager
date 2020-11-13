e3108e4d647b67f0f4e4fc8e688269e101cbe606
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-13 14:33:13 -0800

    fix routing Footer and add pages to appropriate index

--- client/src/pages/User/UserPage.jsx
+++ client/src/pages/User/UserPage.jsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { NavbarUser } from "../common/components";
-import * as components from "../common/components";
-import UserView from './UserView';
+import { Switch, Route} from "react-router-dom";
+import { Footer, NavbarUser, UserProfile } from "../common/components";
+import UserView from "./UserView";
 
-export const UserPage = (props) => {
+export const UserPage = () => {
   return (
     <>
       <NavbarUser />
       <Switch>
-      <Route
-          exact
-          path="/userpage"
-          component={UserView}
-        />
-        <Route          
-          path="/userpage/profile"
-          component={components.UserProfile}
-        />
+        <Route exact path="/userpage" component={UserView} />
+        <Route path="/userpage/profile" component={UserProfile} />
       </Switch>
+      <Footer />
     </>
   );
 };
