3f22f51b4c8782f2761f69022acdec266639989e
Lindsey Smith <campbelllm@gmail.com>
2020-11-13 16:35:35 -0700

    document UserView page, prettify

--- client/src/pages/User/UserView.jsx
+++ client/src/pages/User/UserView.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { useSelector} from 'react-redux';
-import { makeStyles } from "@material-ui/core/styles";
+import { useSelector } from "react-redux";
 // import StatusCard from "../common/components/StatusCard";
 import CreateIssue from "../common/components/CreateIssue";
 import ViewIssues from "../common/components/ViewIssues";
 import NavbarUser from "../common/components/NavbarUser";
-
 import Footer from "../common/components/Footer";
+import { makeStyles } from "@material-ui/core/styles";
 
-
+/**
+ * @description using materialUI makeStyles hook to adjust styling on page
+ */
 const useStyles = makeStyles({
-  root: {
-  },
+  root: {},
   status: {
     margin: "100px 10px",
     display: "flex",
@@ -25,25 +25,31 @@ const useStyles = makeStyles({
   issues: {
     display: "flex",
     justifyContent: "center",
-  }
+  },
 });
-
+/**
+ * @description start of UserView component
+ */
 function UserView() {
-  const visibility = useSelector((state) => {
-    return state.userPage.visibility;
-  });
+  /**
+   * @description getting state for visibility from redux store
+   */
+  const visibility = useSelector((state) => state.userPage.visibility);
+  /**
+   * @description assigning styles from above to a variable for use in elements
+   */
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <NavbarUser></NavbarUser>
 
-      {visibility ? <CreateIssue/>: null}
+      {visibility ? <CreateIssue /> : null}
       {/* <div className={classes.status}>
         <StatusCard title="Total Issues:" />
         <StatusCard title="Open:" />
       </div> */}
       <div className={classes.issues}>
-      <ViewIssues ></ViewIssues>
+        <ViewIssues></ViewIssues>
       </div>
       <div className={classes.footer}>
         <Footer />
