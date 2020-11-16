e4af96afb0bb202be5eab16ddb39d8f746c5947d
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-11-15 16:29:56 -0800

    make partial checkin for viewAllUsers.jsx

--- client/src/pages/Admin/AdminPage.jsx
+++ client/src/pages/Admin/AdminPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 // import "../../styles/App.css";
 import * as components from "../common/components";
-import { AdminCards, ViewAllProjects,CreateProject,CreateUser } from "./views";
+import { AdminCards, ViewAllProjects,CreateProject,CreateUser,ViewAllUsers } from "./views";
 // import  from "./Views/ViewAllProjects";
 
 export function AdminPage() {
@@ -14,6 +14,7 @@ export function AdminPage() {
         <Route exact path="/admin/adminpage/projects" component={ViewAllProjects} />
         <Route path="/admin/adminpage/userprofile" component={components.UserProfile} />
         <Route path="/admin/adminpage/projects/create" component={CreateProject} />
+        <Route exact path="/admin/adminpage/users" component={ViewAllUsers} />
         <Route path="/admin/adminpage/users/create" component={CreateUser} />
       </Switch>
       <components.Footer />
--- client/src/pages/Admin/views/index.jsx
+++ client/src/pages/Admin/views/index.jsx
@@ -2,5 +2,6 @@ import AdminCards from './AdminCards';
 
 export {default as AdminCards} from './AdminCards';
 export {default as ViewAllProjects} from './ViewAllProjects';
+export {default as ViewAllUsers} from './ViewAllUsers'
 export {WrappedCreateProject as CreateProject} from './CreateProject';
 export {WrappedCreateUser as CreateUser} from './CreateUserView';
